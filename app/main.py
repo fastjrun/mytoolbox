a3ed8e1569e101c6bae9a8bff004605891caa63a
fastjrun <fastjrun@139.com>
2023-11-19 18:42:11 +0800

    新增'查看容器实时日志，从最后指定行行开始'功能

--- app/main.py
+++ app/main.py
@@ -2,13 +2,25 @@ import streamlit as st
 
 
 def main():
-    st.title('git更换远程仓库地址')
-    input_text = st.text_input('Enter git-url 👇', key='git地址')
+    st.title('我的工具箱')
+    tabs = st.tabs(["git更换远程仓库地址", "查看容器实时日志，从最后指定行行开始"])
+    with tabs[0]:
+        input_text = st.text_input('Enter git-url 👇', key='git_url')
 
-    # 实时显示连接字符串
-    result_text = 'git remote set-url origin ' + input_text
-    st.write('command👇')
-    st.code(result_text, language='bash')
+        result_text = 'git remote set-url origin ' + input_text
+        st.write('command👇')
+        st.code(result_text, language='bash')
+
+    with tabs[1]:
+        with st.container():
+            col1, col2 = st.columns(2)
+            with col1:
+                container_name_text = st.text_input('输入容器名称 👇', key='container_name')
+            with col2:
+                tails_number = st.text_input('输入指定行 👇', key='tails_number', value='20')
+        result_text = 'docker logs -f --tail= ' + tails_number + ' ' + container_name_text
+        st.write('command👇')
+        st.code(result_text, language='bash')
 
 
 if __name__ == '__main__':
